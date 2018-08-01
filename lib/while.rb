dd3ea11ebd73ed9f935d91b0cbf5cdb291f4a6f1
Tristan Brown <tgbrown@stanford.edu>
2018-08-01 04:27:43 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,6 +2,6 @@ def using_while
 	levitation_force = 6
         while levitation_force < 10
           puts "Wingardium Leviosa"
-          levitation_force +=1
-					break 
+          levitation_force += 1
+					end
 end
